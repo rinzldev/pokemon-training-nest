5775829d2d4dd1c3b9d0830edc126497a401f694
rinzldev <oswaldo.contacto@protonmail.com>
2023-08-21 18:11:22 -0400

    add: multi insert

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -17,17 +17,24 @@ export class SeedService {
   ){}
 
   async runSeed(){
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
 
+    await this.pokemonModel.deleteMany ({}) // para limpiar la bd antes de ejecutar nuevamente
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: {name: string, nro: number} []= []
 
     data.results.forEach(async({name, url}) => {
+      
       const segments = url.split("/")
       const nro = +segments[segments.length - 2]
-      const pokemon = await this.pokemonModel.create({name, nro})
+      //const pokemon = await this.pokemonModel.create({name, nro})
       // console.log(name, nro)
 
+      pokemonToInsert.push({name, nro})
 
     })
+
+    await this.pokemonModel.insertMany(pokemonToInsert)
 
     return 'Seed Executed';
   }
